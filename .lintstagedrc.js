6190f0b9e225f81b5ffdc0c6d3ba2857e3c67f9d
Mike Stecker <mike@stecker.me>
2021-11-15 10:29:10 -0800

    add jest

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -2,5 +2,9 @@ module.exports = {
   '*.{js,ts,tsx}': 'yarn run eslint',
   '*.{js,ts,tsx,css,md}': 'prettier --write',
   '*.{ts,tsx}': () => 'yarn run typecheck',
-  '*.{ts,tsx}': ['eslint . --cache --fix --ext .tsx --ext .ts', () => 'yarn tsc'],
+  '*.{ts,tsx}': [
+    'eslint . --cache --fix --ext .tsx --ext .ts',
+    () => 'yarn tsc',
+    () => 'yarn jest',
+  ],
 }
