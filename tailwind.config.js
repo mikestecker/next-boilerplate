03eb356f06df32d279d400dfa918bff54bbd3116
Mike Stecker <mike@stecker.me>
2021-11-15 09:25:16 -0800

    added styles to purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,9 @@
 module.exports = {
-  purge: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
+  purge: [
+    './src/components/**/*.{css,js,ts,jsx,tsx}',
+    './src/pages/**/*.{css,js,ts,jsx,tsx}',
+    './src/styles/**/*.{css}',
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
