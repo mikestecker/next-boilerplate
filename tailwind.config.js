d678c64e2b1eb9aaa41aba0397744567d6633281
Mike Stecker <mike@stecker.me>
2022-01-14 14:27:27 -0800

    Update Tailwind to 3.0.0

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,11 @@
 module.exports = {
-  purge: [
+  content: [
     './src/components/**/*.{css,js,ts,jsx,tsx}',
     './src/pages/**/*.{css,js,ts,jsx,tsx}',
-    './src/styles/**/*.{css}',
+    './src/styles/**/*.css',
   ],
   darkMode: 'class', // or 'media' or 'class'
   theme: {
-    extend: {},
-  },
-  variants: {
     extend: {},
   },
   plugins: [],
