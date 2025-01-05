cbde73bef4ddd61fa50097f99209d46f9174f2aa
Mike Stecker <mike@stecker.me>
2025-01-05 11:27:07 -0800

    Initial Commit

---
+++ apps/web/app/layout.tsx
@@ -0,0 +1,32 @@
+import '@repo/design-system/styles/globals.css';
+import './styles/web.css';
+import { DesignSystemProvider } from '@repo/design-system';
+import { fonts } from '@repo/design-system/lib/fonts';
+import { cn } from '@repo/design-system/lib/utils';
+import { Toolbar } from '@repo/feature-flags/components/toolbar';
+import type { ReactNode } from 'react';
+import { Footer } from './components/footer';
+import { Header } from './components/header';
+
+type RootLayoutProperties = {
+  readonly children: ReactNode;
+};
+
+const RootLayout = ({ children }: RootLayoutProperties) => (
+  <html
+    lang="en"
+    className={cn(fonts, 'scroll-smooth')}
+    suppressHydrationWarning
+  >
+    <body>
+      <DesignSystemProvider>
+        <Header />
+        {children}
+        <Footer />
+      </DesignSystemProvider>
+      <Toolbar />
+    </body>
+  </html>
+);
+
+export default RootLayout;
