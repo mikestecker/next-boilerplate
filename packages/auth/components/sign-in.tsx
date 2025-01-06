d649b81e214eaf7dad70da66e5b8c4c3b623174c
Mike Stecker <mike@stecker.me>
2025-01-06 15:12:31 -0800

    Update the auth components

--- packages/auth/components/sign-in.tsx
+++ packages/auth/components/sign-in.tsx
@@ -1,11 +1,33 @@
-import { SignIn as ClerkSignIn } from '@clerk/nextjs';
+"use client";
 
-export const SignIn = () => (
-  <ClerkSignIn
-    appearance={{
-      elements: {
-        header: 'hidden',
-      },
-    }}
-  />
-);
+import { signIn } from '../client';
+import { useState } from 'react';
+
+export const SignIn = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  return (
+    <form
+      onSubmit={async (e) => {
+        e.preventDefault();
+        await signIn.email({
+          email,
+          password
+        })
+      }}
+    >
+      <input
+        type="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button type="submit">Sign in</button>
+    </form>
+  );
+}
--- packages/auth/components/sign-up.tsx
+++ packages/auth/components/sign-up.tsx
@@ -1,11 +1,40 @@
-import { SignUp as ClerkSignUp } from '@clerk/nextjs';
+"use client";
 
-export const SignUp = () => (
-  <ClerkSignUp
-    appearance={{
-      elements: {
-        header: 'hidden',
-      },
-    }}
-  />
-);
+import { signUp } from '../client';
+import { useState } from 'react';
+
+export const SignUp = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
+
+  return (
+    <form
+      onSubmit={async (e) => {
+        e.preventDefault();
+        await signUp.email({
+          email,
+          password,
+          name
+        })
+      }}
+    >
+      <input
+        type="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <button type="submit">Sign up</button>
+    </form>
+  );
+}
